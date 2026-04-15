d460413a7ad1c213f4b2ac762cacc5499f887be6
Kaushal-Loya <kaushalloya5@gmail.com>
2026-04-15 12:55:53 +0530

    Add Django project structure with settings and config

---
+++ backend/book_insight/wsgi.py
@@ -0,0 +1,8 @@
+"""
+WSGI config for book_insight project.
+"""
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'book_insight.settings')
+application = get_wsgi_application()
